45c8e7ce7163d72568cbb23d1d18f2cef5692a9c
Tailane Isaias <147218987+tailaneisaias@users.noreply.github.com>
2025-09-22 00:34:25 +0000

    Imprimindo as respostas da carta1  e  retornando os dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,44 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+int main()
+{
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  char Estado;
+  char CodigoDaCarta[20];
+  char NomeDaCidade[50];
+
+  int Populacao, NumeroDePontosTuristicos;
+  float Area, PIB;
+
   // Área para entrada de dados
+  printf("Carta1 \n");
+
+  printf("Uma letra que represente seu estado: \n");
+  scanf("%c", &Estado);
+
+  printf("Digite a letra do seu estado, junto com um número de 01 a 04 (ex: C01, B02) \n");
+  scanf("%s", CodigoDaCarta);
+
+  printf("Nome da sua cidade: \n");
+  scanf("%s", NomeDaCidade);
+
+  printf("O número de habitantes da sua cidade: \n");
+  scanf("%d", &Populacao);
+
+  printf("A aréa da cidade em quilometros quadrados \n");
+  scanf("%f", &Area);
+
+  printf("O produto interno Bruto da cidade \n");
+  scanf("%f", &PIB);
+
+  printf("A quantidade de pontos turisticos da sua cidade: \n");
+  scanf("%i", &NumeroDePontosTuristicos);
 
   // Área para exibição dos dados da cidade
+  printf("-Estado: %c \n-CodigoDaCarta: %s \n-NomeDaCidade: %s \n-População: %d\n", Estado, CodigoDaCarta, NomeDaCidade, Populacao);
+  printf("-Area: %f \n-PIB: %f \n-NúmeroDePontosTurísticos: %i \n", Area, PIB, NumeroDePontosTuristicos);
 
-return 0;
-} 
+  return 0;
+}
