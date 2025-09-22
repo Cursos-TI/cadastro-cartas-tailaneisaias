232a264cad7c3526f3ef54c6d4bd26668f5d321a
Tailane Isaias <147218987+tailaneisaias@users.noreply.github.com>
2025-09-22 00:50:39 +0000

    Adicionando nova funcionalidade para entrada de dados da Carta2 e ajustando a formatação do código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,7 +15,7 @@ int main()
   int Populacao, NumeroDePontosTuristicos;
   float Area, PIB;
 
-    // Área para entrada de dados
+  // Área para entrada de dados
   printf("Carta1 \n");
 
   printf("Uma letra que represente seu estado: \n");
@@ -42,6 +42,39 @@ int main()
   // Área para exibição dos dados da cidade
   printf("-Estado: %c \n-CodigoDaCarta: %s \n-NomeDaCidade: %s \n-População: %d\n", Estado, CodigoDaCarta, NomeDaCidade, Populacao);
   printf("-Area: %f \n-PIB: %f \n-NúmeroDePontosTurísticos: %i \n", Area, PIB, NumeroDePontosTuristicos);
+
+  char NomeDoEstado;
+  char Codigo[30];
+  char Cidade[40];
+
+  int Habitantes, Turismo;
+  float AreaDaCidade, PibDacidade;
+
+  printf("Carta2 \n");
+
+  printf(" - Digite uma letra de 'A' a 'H' que represente seu estado: \n");
+  scanf(" %c", &NomeDoEstado);
+
+  printf(" - A letra do seu estado seguida de número de 01 a 04(Ex: A02, B03): \n");
+  scanf(" %s", Codigo);
+
+  printf(" - Digite o nome da sua cidade: \n");
+  scanf("%s", Cidade);
+
+  printf(" - Número de habitantes da sua cidade: \n");
+  scanf("%d", &Habitantes);
+
+  printf("- A quantidade de pontos turisticos da sua cidade: \n");
+  scanf("%i", &Turismo);
+
+  printf(" - A área da sua cidade em quilomêtros quadrados: \n");
+  scanf("%f", &AreaDaCidade);
+
+  printf(" - O produto interno Bruto da cidade: \n");
+  scanf("%f", &PibDacidade);
+
+  printf("-estado: %c \n-codigodacarta: %s \n-nomedacidade %s \n-populacao: %d \n", NomeDoEstado, Codigo, Cidade, Habitantes);
+  printf("-numerodepontosturisticos: %i \n-area: %f \n-pib: %f \n", Turismo, AreaDaCidade, PibDacidade);
 
   return 0;
 }
