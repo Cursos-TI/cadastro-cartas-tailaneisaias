715d606b753efb537453d1b14b9dbb13b9660e37
Tailane Isaias <147218987+tailaneisaias@users.noreply.github.com>
2025-09-27 01:21:13 +0000

    Ajustando tipos de variáveis e adicionando cálculo de Super Poder, além de implementar comparação entre cartas.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,8 +12,9 @@ int main()
   char CodigoDaCarta[20];
   char NomeDaCidade[50];
 
-  int Populacao, NumeroDePontosTuristicos;
-  float Area, PIB, DensidadePopulacional, PibPerCapita;
+  unsigned long int Populacao;
+  int NumeroDePontosTuristicos;
+  float Area, PIB, DensidadePopulacional, PibPerCapita, SuperPoder;
 
   // Área para entrada de dados
   printf("Carta1 \n");
@@ -41,17 +42,19 @@ int main()
 
   DensidadePopulacional = (float)(Populacao / Area);
   PibPerCapita = (float)(PIB / Populacao);
+  SuperPoder = (float)Populacao + Area + PIB + NumeroDePontosTuristicos + PibPerCapita + (1.0f / DensidadePopulacional);
 
   // Área para exibição dos dados da cidade
   printf("-Estado: %c \n-CodigoDaCarta: %s \n-NomeDaCidade: %s \n-População: %d\n", Estado, CodigoDaCarta, NomeDaCidade, Populacao);
-  printf("-Area: %.2f \n-PIB: %.2f \n-NúmeroDePontosTurísticos: %i \n-Densidade populacional: %.2f hab/km² \n-PIB per capita: %.2f reais\n", Area, PIB, NumeroDePontosTuristicos, DensidadePopulacional, PibPerCapita);
+  printf("-Area: %.2f \n-PIB: %.2f \n-NúmeroDePontosTurísticos: %i \n-Densidade populacional: %.2f hab/km² \n-PIB per capita: %.2f reais\n-Super Poder: %.2f\n", Area, PIB, NumeroDePontosTuristicos, DensidadePopulacional, PibPerCapita, SuperPoder);
 
   char NomeDoEstado;
   char Codigo[30];
   char Cidade[40];
 
-  int Habitantes, Turismo;
-  float AreaDaCidade, PibDacidade, Densidadepopulacional, PIBPercapita;
+  unsigned long int Habitantes;
+  int Turismo;
+  float AreaDaCidade, PibDacidade, Densidadepopulacional, PIBPercapita, Superpoder;
 
   printf("Carta2 \n");
 
@@ -78,9 +81,19 @@ int main()
 
   Densidadepopulacional = (float)(Habitantes / AreaDaCidade);
   PIBPercapita = (float)(PibDacidade / Habitantes);
+  Superpoder = (float)Habitantes + AreaDaCidade + PibDacidade + Turismo + PIBPercapita + (1.0f / Densidadepopulacional);
 
   printf("-estado: %c \n-codigodacarta: %s \n-nomedacidade %s \n-populacao: %d \n", NomeDoEstado, Codigo, Cidade, Habitantes);
-  printf("-numerodepontosturisticos: %i \n-area: %f \n-pib: %.2f \n-DensidadePopulacional: %.2f hab/km²\n-PIB per capita: %.2f reais\n", Turismo, AreaDaCidade, PibDacidade, Densidadepopulacional, PIBPercapita);
+  printf("-numerodepontosturisticos: %i \n-area: %f \n-pib: %.2f \n-DensidadePopulacional: %.2f hab/km²\n-PIB per capita: %.2f reais\n-Super Poder: %2f\n", Turismo, AreaDaCidade, PibDacidade, Densidadepopulacional, PIBPercapita, Superpoder);
+
+  printf("\nComparação de Cartas:\n");
+  printf("População: Carta 1 venceu (%d)\n", Populacao > Habitantes);
+  printf("Área: Carta 1 venceu (%d)\n", Area > AreaDaCidade);
+  printf("PIB: Carta 1 venceu (%d)\n", PIB > PibDacidade);
+  printf("Pontos Turísticos: Carta 1 venceu (%d)\n", NumeroDePontosTuristicos > Turismo);
+  printf("Densidade Populacional: Carta 1 venceu (%d)\n", DensidadePopulacional < Densidadepopulacional);
+  printf("PIB per Capita: Carta 1 venceu (%d)\n", PibPerCapita > PIBPercapita);
+  printf("Super Poder: Carta 1 venceu (%d)\n", SuperPoder > Superpoder);
 
   return 0;
 }
